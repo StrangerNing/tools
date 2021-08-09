1c86c50cc11fd9c8f0e0f49b7dceba5dc5d05783
zhuzening <tsening.cn@gmail.com>
2021-08-09 18:20:10 +0800

    后台管理友链状态枚举更新

--- tools-fe/vue/src/views/blog/blogEnums.js
+++ tools-fe/vue/src/views/blog/blogEnums.js
@@ -6,5 +6,5 @@ export default {
   commentLimitEnum: new Enum().add('enable', '允许', 1).add('disable', '不允许', 0),
   articleStatusEnum: new Enum().add('normal', '正常', 1).add('draft', '草稿', 2).add('delay', '定时', 3).add('deleted', '删除', 0),
   articleTypeEnum: new Enum().add('default', '默认', 1).add('big', '大图', 2).add('left', '左图', 3).add('sidebar', '侧边栏', 4).add('images', '图片模式', 5),
-  friendsLinkEnum: new Enum().add('wait', '待审核', 1).add('approve', '审核通过', 2).add('invalidate', '链接失效', 3).add('removed', '被对方移除', 4).add('repeat', '重复申请', 5).add('deny', '审核不通过', 6)
+  friendsLinkEnum: new Enum().add('wait', '待审核', 100).add('approve', '审核通过', 200).add('invalidate', '链接失效', 301).add('removed', '被对方移除', 302).add('repeat', '重复申请', 400).add('deny', '审核不通过', 500)
 }
